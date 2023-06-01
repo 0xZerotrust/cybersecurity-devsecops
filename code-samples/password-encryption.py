f949bd9dd4cd0cf42b5659244ceb5ad85a418a3f
0xZerotrust <135234322+0xZerotrust@users.noreply.github.com>
2023-06-01 15:10:23 +0200

    Create password-encryption.py

---
+++ code-samples/password-encryption.py
@@ -0,0 +1,12 @@
+# Python Script for Password Encryption
+
+# Implement password encryption using a secure hashing algorithm
+def encrypt_password(password):
+    # Add encryption logic here
+    encrypted_password = ... # Encrypted password
+    return encrypted_password
+
+# Example usage
+password = input("Enter your password: ")
+encrypted_password = encrypt_password(password)
+print("Encrypted password:", encrypted_password)
